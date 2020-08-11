889f13497fdbb30321856a108110ef7331a6a9ad
avoclue <68810851+avoclue@users.noreply.github.com>
2020-08-11 17:48:40 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="index.css">
+    <title>Pengumuman SBMPTN 2020</title>
+</head>
+<body>
+    <!-- <script>
+        alert("scan codenya");
+    </script>     -->
+
+    <nav>
+        <div class="container">
+            <h1 class="logo">BUKAN LTMPT</h1>
+            <p>Bukan Lembaga Tes Masuk Perguruan Tinggi</p>
+        </div>
+    </nav>
+
+    <main>
+        <div class="container">
+            <h2>PENGUMUMAN HASIL SELEKSI SBMPTN LTMPT 2020</h2>
+
+            <div class="res">
+                <div class="qr">
+                    <img src="qr.png">
+                </div>
+
+                <div class="data">
+                    <table>    
+                        <tr>
+                            <td>Nomor Peserta</td>
+                            <td>:</td>
+                            <td>12</td>
+                        </tr>
+
+                        <tr>
+                            <td>Nama</td>
+                            <td>:</td>
+                            <td>N</td>
+                        </tr>
+
+                        <tr>
+                            <td>Tanggal lahir</td>
+                            <td>:</td>
+                            <td>11</td>
+                        </tr>
+                    </table>    
+
+                    <p class="selamat">Selamat! Anda dinyatakan lulus seleksi SBMPTN LTMPT 2020 di</p>
+
+                    <table>
+                        <tr>
+                            <td>PTN</td>
+                            <td>:</td>
+                            <td>ITS</td>
+                        </tr>
+
+                        <tr>
+                            <td>Program Studi</td>
+                            <td>:</td>
+                            <td>A</td>
+                        </tr>
+                    </table>
+
+                    <!-- <p class="bidikmisi">Anda sebagai pemohon program <span>BIDIKMISI</span>, keputusan <span>DITERIMA</span> atau <span>TIDAKNYA</span> permohonan program <span>BIDIKMISI</span> anda akan disampaikan oleh Rektor PTN di mana anda diterima berdasarkan hasil VERIFIKASI</p> -->
+
+                    <p>Persyaratan pendaftaran ulang calon mahasiswa baru dapat dilihat di <a href="#">sini</a>.</p>             
+
+                    <p>Anda dapat mencetak kembali Kartu Tanda Peserta SBMPTN 2020 di <a href="#">sini</a>.</p>
+
+                    <a href="#" class="btn unduh">UNDUH SK PESERTA LULUS SBMPTN 2020 (PDF)</a>
+
+                </div>
+
+                <div class="back">
+                    <a href="#" class="btn kembali">Kembali ke pancarian</a>
+                </div>
+
+            </div>
+        </div>
+    </main>
+
+</body>
+</html>
